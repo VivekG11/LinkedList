06e07a9b9986bf022f9e0ff0cb3b9fa782bc554c
VivekG11 <vivekvk2903@gmail.com>
2021-07-10 16:02:49 +0530

    [Simple Linked list]

---
+++ LinkedList/List.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    public class List
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if ((this.head) == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(node.data + " is added to the list.");
+        }
+        public void Display()
+        {
+            Console.WriteLine("Elements in the list are ..");
+            Node temp = this.head;
+            if(temp == null)
+            {
+                Console.WriteLine("List is Empty..");
+            }
+            while(temp!= null)
+            {
+                Console.Write(temp.data+",");
+                temp = temp.next;
+            }
+        }
+      
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List........");
+            List list = new List();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
